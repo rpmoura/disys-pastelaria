10b08c4defe1c5835cd19f9482066faad8acff10
Reginaldo Moura <reginaldo.mourap@gmail.com>
2023-12-10 00:07:58 -0400

    feat: add exception message for product.

--- lang/en/exception.php
+++ lang/en/exception.php
@@ -5,12 +5,15 @@ return [
     'client'             => [
         'not_found' => 'Client not found.',
     ],
-    'file_manager'       => [
+    'file_manager' => [
         'upload' => [
             'fail' => 'Failed to upload file.',
         ],
         'delete' => [
             'fail' => 'Failed to delete the file',
         ],
+    ],
+    'product' => [
+        'not_found' => 'Product not found.',
     ],
 ];
