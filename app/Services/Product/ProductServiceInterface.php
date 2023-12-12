c74fb6d7df2f8a60deba900434ca550a6319a61d
Reginaldo Moura <reginaldo.mourap@gmail.com>
2023-12-12 01:40:54 -0400

    feat: add findBy method in product service.

--- app/Services/Product/ProductServiceInterface.php
+++ app/Services/Product/ProductServiceInterface.php
@@ -4,15 +4,19 @@ namespace App\Services\Product;
 
 use App\Models\Product;
 use App\Repositories\Product\ProductRepositoryInterface;
+use Illuminate\Support\Collection;
 
 interface ProductServiceInterface
 {
     public function create(array $attributes): Product;
+
     public function update(Product $product, array $attributes): Product;
+
     public function findOneBy(string $key, int|string $value): Product;
 
     public function findList(): ProductRepositoryInterface;
 
     public function delete(Product $product): bool;
 
+    public function findBy(string $key, mixed $values): Collection;
 }
